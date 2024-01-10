7bec4005c9c88a17560fbc1441949cab9003fc71
valenium <valenium@live.com>
2024-01-10 13:24:53 -0600

    part 2 complete

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('index/', views.dogs_index, name='index')
+    path('dogs/', views.dogs_index, name='index'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
-dogs = [
-    {'name': 'Chewy', 'breed': 'Korean Jindo', 'age': 6},
-    {'name': 'Daisy', 'breed': 'Sheltie/Australian Shepherd', 'age': 6},
-    {'name': 'Kylo', 'breed': 'Mini Australian Shepherd', 'age': 5},
-]
+# dogs = [
+#     {'name': 'Chewy', 'breed': 'Korean Jindo', 'age': 6},
+#     {'name': 'Daisy', 'breed': 'Sheltie/Australian Shepherd', 'age': 6},
+#     {'name': 'Kylo', 'breed': 'Mini Australian Shepherd', 'age': 5},
+# ]
+
+from .models import Dog
+
 
 # Create your views here.
 def home(request):
@@ -14,6 +17,13 @@ def about(request):
     return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {
         'dogs': dogs
+    })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/details.html', {
+        'dog': dog
     })
